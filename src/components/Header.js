328e7bcca31971b0c3f9b677cf43aed6c2cf6f34
Vladimir <timotijevicvlada@gmail.com>
2021-10-10 18:38:35 +0200

    Customized for mobile screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = ({setSearch}) => {
     return (
         <div className="header">
-            <h2 className="logo">Crypto App</h2>
+            <h1 className="logo">Crypto App</h1>
             <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="search coin"/>
         </div>
     )
